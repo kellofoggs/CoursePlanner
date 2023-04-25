47ab80d8643429da71f47d0ddc429ab6e4ea0e08
kellofoggs <kojukwu2003@gmail.com>
2023-04-25 15:04:41 -0700

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+public class Program {
+
+    private Course start;
+    private Course end;
+    //Graph of courses
+    private Course[] requiredClasses;
+    private double totalGPA;
+
+    public static Program generateProgram(String sourceURL){
+        File sourceFile = new File()
+
+    }
+
+
+    public Program(Course [] courses){
+        requiredClasses = courses;
+
+
+
+
+
+
+
+    }
+
+
+}
